608d16b0c4a5a8977f9028ef5ad1888ff96718fa
Abhay Narayan <abhaynarayan16122002@gmail.com>
2024-03-17 12:55:57 +0530

    first commit

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignIn } from '@clerk/nextjs'
+import React from 'react'
+
+const SignIpPage = () => {
+  return (
+    <SignIn/>
+  )
+}
+
+export default SignIpPage
---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const IBM = IBM_Plex_Sans({ subsets: ["latin"],
+  weight:['400','500','600','700'],
+  variable:'--font-ibm-plex'
+});
+
+export const metadata: Metadata = {
+  title: "Alphax",
+  description: "AO- power image generator",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={cn('font-IBMPlex antialiased', IBM.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
