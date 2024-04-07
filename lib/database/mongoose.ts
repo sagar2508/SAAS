62ce1aa152a50496ec70b99dc74ba10f2af50017
sagar2508 <tanwarsagar936@gmail.com>
2024-04-07 14:44:09 +0530

    modified route

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,6 +1,6 @@
 import mongoose, { Mongoose } from 'mongoose';
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGO_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
   conn: Mongoose | null;
@@ -18,15 +18,15 @@ if(!cached) {
 export const connectToDatabase = async () => {
   if(cached.conn) return cached.conn;
 
-  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+  if(!MONGO_URL) throw new Error('Missing MONGODB_URL');
+  
 
   cached.promise = 
     cached.promise || 
-    mongoose.connect(MONGODB_URL, { 
+    mongoose.connect(MONGO_URL, { 
       dbName: 'SAAS', bufferCommands: false 
     })
 
   cached.conn = await cached.promise;
-
   return cached.conn;
 }
